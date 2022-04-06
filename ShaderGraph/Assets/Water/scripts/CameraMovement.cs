c3d26855bf8e170f725e64c24892be2e62b7275a
eungyukm <eungyukm@gmail.com>
2022-04-06 20:38:49 +0900

    Project Upload

--- ShaderGraph/Assets/Water/scripts/CameraMovement.cs
+++ ShaderGraph/Assets/Water/scripts/CameraMovement.cs
@@ -28,32 +28,28 @@ public class CameraMovement : MonoBehaviour
 
     private void Update()
     {
-        // Only work with the Left Alt pressed
-        if (Input.GetKey(KeyCode.LeftAlt))
+        //Look around with Left Mouse
+        if (Input.GetMouseButton(0))
         {
-            //Look around with Left Mouse
-            if (Input.GetMouseButton(0))
-            {
-                this.yaw += this.lookSpeedH * Input.GetAxis("Mouse X");
-                this.pitch -= this.lookSpeedV * Input.GetAxis("Mouse Y");
+            this.yaw += this.lookSpeedH * Input.GetAxis("Mouse X");
+            this.pitch -= this.lookSpeedV * Input.GetAxis("Mouse Y");
 
-                this.transform.eulerAngles = new Vector3(this.pitch, this.yaw, 0f);
-            }
-
-            //drag camera around with Middle Mouse
-            if (Input.GetMouseButton(2))
-            {
-                transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
-            }
-
-            if (Input.GetMouseButton(1))
-            {
-                //Zoom in and out with Right Mouse
-                this.transform.Translate(0, 0, Input.GetAxisRaw("Mouse X") * this.zoomSpeed * .07f, Space.Self);
-            }
-
-            //Zoom in and out with Mouse Wheel
-            this.transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * this.zoomSpeed, Space.Self);
+            this.transform.eulerAngles = new Vector3(this.pitch, this.yaw, 0f);
         }
+
+        //drag camera around with Middle Mouse
+        if (Input.GetMouseButton(2))
+        {
+            transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * dragSpeed, 0);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            //Zoom in and out with Right Mouse
+            this.transform.Translate(0, 0, Input.GetAxisRaw("Mouse X") * this.zoomSpeed * .07f, Space.Self);
+        }
+
+        //Zoom in and out with Mouse Wheel
+        this.transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * this.zoomSpeed, Space.Self);
     }
 }
