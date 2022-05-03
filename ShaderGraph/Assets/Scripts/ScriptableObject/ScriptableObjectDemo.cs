5f2bfd83d8b572a4ad67bae21f288b10ba9c5424
eungyukm <eungyukm@gmail.com>
2022-05-03 17:57:14 +0900

    정리

--- ShaderGraph/Assets/Scripts/ScriptableObject/ScriptableObjectDemo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObject/SODemo", fileName = "SODemo")]
-public class ScriptableObjectDemo : ScriptableObject
-{
-    public int value;
-}
